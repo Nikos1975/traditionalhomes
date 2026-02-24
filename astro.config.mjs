df497bc425f81608c82eeaf750e4618842de4005
Nikuno <59173076+Nikos1975@users.noreply.github.com>
2026-02-24 10:24:50 +0200

    Remove sitemap integration from astro.config.mjs
    Removed sitemap integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()], // remove sitemap()
   output: 'static',
   trailingSlash: 'always',
   build: { format: 'directory' },
